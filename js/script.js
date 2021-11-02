b2e5b8de9d67e912618a881d7a9b14cbd5d99199
Daniel_Alvarado <danielalvarado1498@gmail.com>
2021-11-01 21:06:10 -0600

    Second commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+
+
+
+$.ajax({
+    method:'GET',
+    url:'https://test1-3bba9-default-rtdb.firebaseio.com/',
+    data: JSON.stringify({}),
+    success:(response) => {
+
+    },
+    error:(error) => {
+
+    },
+
+    async:false,
+})
